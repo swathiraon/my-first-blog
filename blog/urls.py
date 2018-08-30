9e7966dc52de917e0f277f55df1deb909799acdc
swathiraon <swat.snr98@gmail.com>
2018-08-31 00:48:27 +0530

    Modificayion in url.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
